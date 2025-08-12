63957180039c6f09b541d10d716e8921ea61b00d
Rishitha13 <72331830+Rishitha13@users.noreply.github.com>
2025-08-12 12:07:47 +0530

    Update streamlit_app.py
    added chages

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+streamlit.title("My Parents new healthy Dinner")
 st.write(
   """Choose the fruits you want in your custom smoothie!
   """
@@ -46,3 +46,6 @@ if ingredients_list:
     if ingredients_string:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!' + name_on_order, icon="✅")
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
